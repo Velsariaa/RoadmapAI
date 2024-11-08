2b994f9e78e9f68a3c1737800a05c0b1c4ff55c4
guiribajustin2004 <127721330+guiribajustin2004@users.noreply.github.com>
2024-11-08 21:21:33 +0800

    Update index.php

--- Backend/index.php
+++ Backend/index.php
@@ -1,3 +1,11 @@
 <?php
     require("codes/pages/routing.php");
+
+    // // The command to run your Python script
+    // $command = 'python main.py'; // Adjust the Python command based on your setup
+
+    // // Run the Python script and capture the output
+    // $output = shell_exec($command);
+
+
 ?>
