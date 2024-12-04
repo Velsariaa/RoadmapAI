cf47f66c8a33e222f585a464c150d659e4f4ba08
RANEASHBELL <kennyranes2319@gmail.com>
2024-12-04 21:47:22 +0800

    Vue3 bootstrap on landing page
    Converted Landing page html to Vue3 bootstrap + responsive

--- Frontend/RoadmapAI/src/main.js
+++ Frontend/RoadmapAI/src/main.js
@@ -1,12 +1,17 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // Vue Router
+import BootstrapVue3 from 'bootstrap-vue-3';
+
+// Import Bootstrap and BootstrapVue styles
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
-import { createApp } from 'vue';
-import App from './App.vue';
-import BootstrapVue3 from 'bootstrap-vue-3';
-import router from './router';
-
 const app = createApp(App);
+
+
+app.use(router);
 app.use(BootstrapVue3);
 
-createApp(App).use(router).mount('#app');
+
+app.mount('#app');
