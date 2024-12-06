b0b51ee28b0e739034c4001cead66c5da9b8f3a6
guiribajustin2004 <127721330+guiribajustin2004@users.noreply.github.com>
2024-12-07 00:03:21 +0800

    [New] Backend for dashboardpick

---
+++ Backend/API/dashboard/Roadmap.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, Cookie");
+header("Access-Control-Allow-Credentials: true");
+require("codes/others/connection.php");
+
+?>
